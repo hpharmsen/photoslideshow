82802bbe54d75a71b0675d5f2cb010142b67b420
HP Harmsen <hph@oberon.nl>
2020-04-13 13:35:39 +0200

    Settings from ini file

--- slideshow.py
+++ slideshow.py
@@ -8,13 +8,13 @@ import random
 import os
 import sys
 import time
+
+from configparser import ConfigParser
 from PIL import ExifTags, Image
 
 import pyglet
 from pyglet.window import key
 
-TIME_PER_PICTURE = 7.0
-FRAMERATE = 60.0
 FILETYPES = (
     '.bmp',
     '.dib',
@@ -57,7 +57,7 @@ def adjust_rotation(image_file):
 
 
 class Photo:
-    def __init__(self, window, image_file):
+    def __init__(self, window, image_file, animate, time_per_picture=None):
         self.window = window
         adjust_rotation(image_file)
         self.image_file = image_file
@@ -69,24 +69,52 @@ class Photo:
 
         self.sprite.x = 0
         self.sprite.y = 0
+        self.time_per_picture = time_per_picture
 
         self.direction = 'None'
         if self.width / self.height < window.width / window.height:
             self.direction = 'Vertical'
-            self.sprite.scale = float(window.width) / self.width
-            self.pan_dist = self.height * self.sprite.scale - window.height
-            self.sprite.y = window.height - self.height * self.sprite.scale
+            if not animate:
+                self.sprite.scale = window.height / self.height * 1.2 # 20% greater than window height
+                self.sprite.y = (window.height - self.height * self.sprite.scale) / 2
+                self.sprite.x = (window.width - self.width * self.sprite.scale) / 2
+            else:
+                self.sprite.scale = float(window.width) / self.width
+                self.pan_dist = self.height * self.sprite.scale - window.height
+                self.sprite.y = window.height - self.height * self.sprite.scale
         else:  # Landscape mode
             self.direction = 'Horizontal'
-            self.sprite.scale = float(window.height) / self.height
-            self.pan_dist = self.width * self.sprite.scale - window.width
+            if not animate:
+                self.sprite.scale = window.width / self.width * 1.2
+                self.sprite.x = (window.width - self.width * self.sprite.scale) / 2
+                self.sprite.y = (window.height - self.height * self.sprite.scale) / 2
+            else:
+                self.sprite.scale = float(window.height) / self.height
+                self.pan_dist = self.width * self.sprite.scale - window.width
+
+        image_dir = image_file.split('/')[-2]
+        try:
+            year = int(image_dir[:4])
+            name = image_dir[5:]
+            if name.lower() == 'diversen':
+                image_dir = str(year)
+            else:
+                image_dir = name + ', ' + str(year)
+        except:
+            pass
+        self.label = pyglet.text.Label(image_dir,
+                                  font_name='Times New Roman',
+                                  font_size=24,
+                                  x=window.width - 10, y= 30 ,
+                                  anchor_x='right', anchor_y='center')
+
         self.start_time = time.time()
 
     def update_pan(self):
         if self.direction == 'None':
             return
         elapsed_time = time.time() - self.start_time
-        fraction = min(1, elapsed_time / TIME_PER_PICTURE)
+        fraction = min(1, elapsed_time / self.time_per_picture)
         if self.direction == 'Horizontal':
             self.sprite.x = -fraction * self.pan_dist
         else:
@@ -95,11 +123,18 @@ class Photo:
     def update_zoom(self):
         self.sprite.scale += 0.0002
 
+    def draw(self):
+
+        self.sprite.draw()
+        elapsed_time = time.time() - self.start_time
+        if self.time_per_picture - elapsed_time < 2:
+            self.label.draw()
 
 class Show:
-    def __init__(self, mainpath):
-        self.images = self.load(mainpath)
+    def __init__(self, config):
+        self.images = self.load(config['imagedir'])
         self.paused = False
+        self.config = config
 
     def load(self, mainpath):
         image_file = 'images.txt'
@@ -124,7 +159,7 @@ class Show:
             if self.paused:
                 return
             img_path = random.choice(self.images)
-        photo = Photo(window, img_path)
+        photo = Photo(window, img_path, self.config['animate'], self.config['time_per_picture'])
         window.clear()
         return photo
 
@@ -136,7 +171,7 @@ window = pyglet.window.Window(fullscreen=True and not debugging)
 
 @window.event
 def on_draw():
-    cur_photo.sprite.draw()
+    cur_photo.draw()
 
 
 @window.event
@@ -172,16 +207,30 @@ def new_image(time):
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) > 1, 'Specify path'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dir', help='directory of images', nargs='?', default=os.getcwd())
-    args = parser.parse_args()
+    parser = ConfigParser()
+    parser.read(os.path.join(os.path.dirname(os.path.abspath(__file__)),'slideshow.ini'))
+    ini = parser['slideshow']
+    imagedir = ini.get('imagedir')
 
-    show = Show(args.dir)
+    if not imagedir:
+        assert len(sys.argv) > 1, 'Spcify image directory in slideshow.ini or pass it as a parameter'
+        parser = argparse.ArgumentParser()
+        parser.add_argument('dir', help='directory of images', nargs='?', default=os.getcwd())
+        args = parser.parse_args()
+        imagedir = args.dir
+
+    config = {'imagedir':imagedir,
+              'time_per_picture': float(ini.get('time_per_picture',7)),
+              'animate': int(ini.get('animate',1)),
+              'framerate': float(ini.get('framerate',60))
+    }
+
+    show = Show(config)
 
     cur_photo = show.new_image()
-    pyglet.clock.schedule_interval(new_image, TIME_PER_PICTURE)
-    pyglet.clock.schedule_interval(update_pan, 1 / FRAMERATE)
-    pyglet.clock.schedule_interval(update_zoom, 1 / FRAMERATE)
+    pyglet.clock.schedule_interval(new_image, config['time_per_picture'])
+    if config['animate']:
+        pyglet.clock.schedule_interval(update_pan, 1 / config['framerate'])
+        pyglet.clock.schedule_interval(update_zoom, 1 / config['framerate'])
 
     pyglet.app.run()
